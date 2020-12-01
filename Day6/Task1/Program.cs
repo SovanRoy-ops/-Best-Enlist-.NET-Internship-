dd4269d78b54356f0c9897c04783457b69b4ab27
SovanRoy-ops <75088235+SovanRoy-ops@users.noreply.github.com>
2020-12-01 18:33:27 +0530

    Day6 tasks added

---
+++ Day6/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+
+
+        /* Given a string, write a method that counts its number of words. Assume there are no leading and trailing whitespaces and there is only single whitespace between two consecutive words.
+Expected input and output -NumberOfWords("This is sample sentence") → 4 (output)
+NumberOfWords("OK") → 1(output)  */
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfWords("Hii! this is a sentence!"));  //5
+        }
+
+
+
+        static int NumberOfWords(string s){
+            
+            int count = 0;
+            string[] words = s.Split(" ");
+            foreach (string word in words)
+                 {
+                    count++;
+                }
+                return count;
+        }
+
+
+
+    }
+}
---
+++ Day6/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /* Given a string, write a method that checks if it is a palindrome. String length may be >= 0.
+
+Expected input and output IsPalindrome("tat") → true IsPalindrome("hello") → false
+            */
+
+
+
+            Console.WriteLine(IsPalindrome("tat"));  // true
+             Console.WriteLine(IsPalindrome("hello")); // false
+        }
+
+
+        static bool IsPalindrome(string s){
+
+            string revs ="";
+
+           for (int i = s.Length-1; i >=0; i--) 
+            {  
+                revs += s[i].ToString();  
+            }  
+         
+         
+         
+            return (revs == s);
+          
+           
+
+
+
+        }
+
+
+
+    }
+}
