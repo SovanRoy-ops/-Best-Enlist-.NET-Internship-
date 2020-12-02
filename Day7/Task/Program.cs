a0016ae392e51ba5d07c6b505c05864712ad100d
SovanRoy-ops <75088235+SovanRoy-ops@users.noreply.github.com>
2020-12-02 21:56:44 +0530

    Day7 Task added

---
+++ Day7/Task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+    /* Write a method that replaces every letter 'y' in the string with 'x'. Assume that string contains only lower case letters.
+
+            Expected input and output
+            BEReplace("yellow") → "xellow"
+            BEReplace("mushroom") → "mushroom”  */
+
+            Console.WriteLine(BEReplace("yellow"));  // "xellow"
+            Console.WriteLine(BEReplace("mushroom")); // "mushroom"
+        }
+
+        static string BEReplace(string s){
+
+                return s.Replace('y','x');
+        }
+
+
+
+    }
+}
