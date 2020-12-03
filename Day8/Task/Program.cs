99275c1db24770b985bf305e128565c873395231
SovanRoy-ops <75088235+SovanRoy-ops@users.noreply.github.com>
2020-12-03 14:52:58 +0530

    Day8 Task added

---
+++ Day8/Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Given an array of 3 integers, write a method that checks if multiplication or sum of two first numbers is greater than third one.
+            Expected input and output
+            CheckGreaterNumber([2, 7, 12]) → true
+            CheckGreaterNumber([-5, -8, 50]) → false
+            */
+
+
+            Console.WriteLine(CheckGreaterNumber(new int[] {2, 7, 12}));  // True
+            Console.WriteLine(CheckGreaterNumber(new int[] {-5, -8, 50}));   // False
+        }
+
+
+        static bool CheckGreaterNumber(int[] arr){
+
+                return (arr[0]+arr[1]>arr[2] || arr[0]*arr[1]>arr[2]);
+
+        }
+
+
+    }
+}
