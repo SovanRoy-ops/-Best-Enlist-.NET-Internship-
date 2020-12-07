fb4a8844d7dd9dd0b6d6b2f8daec185959f960b0
SovanRoy-ops <75088235+SovanRoy-ops@users.noreply.github.com>
2020-12-07 23:00:05 +0530

    Day12 Tasks added

---
+++ Day12/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a program in C# Sharp to find the prime numbers within a range of numbers
+Test Data :Input starting number of range: 1Input ending number of range : 50Expected Output :The prime number between 1 and 50 are :2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+            */
+
+            int flag  = 0;
+            Console.Write("Enter the range: ");
+            int n = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 2; i <= n; i++)
+            {
+                int j = 2;
+                flag = 0;
+                while(j<=(i/2))
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                    j++;
+                }
+
+                if(flag==0)
+                    Console.Write(i + " ");
+
+            }
+            
+        }
+    }
+}
---
+++ Day12/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        /* Write a program in C# Sharp to display the number in reverse order. Go to the editorTestData :Input a number: 12345Expected Output :The number in reverse order is : 54321 */
+
+
+            int  n, reverse=0, rem;           
+            Console.Write("Enter a number: ");      
+            n = int.Parse(Console.ReadLine());     
+            while(n!=0)      
+                {      
+                rem=n%10;        
+                reverse=reverse*10+rem;      
+                n/=10;      
+                }      
+            Console.Write("Reversed Number: "+reverse); 
+        }
+    }
+}
