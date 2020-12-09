fdc1cb4bf2fdd0d932aefc0fae1659327a1af897
SovanRoy-ops <75088235+SovanRoy-ops@users.noreply.github.com>
2020-12-09 23:54:51 +0530

    Day14 Tasks added

---
+++ Day14/Task/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        /* Write a c# program to achieve operator overloading */
+
+        bank ba1 = new bank(10,-20);
+        ba1.display();
+        bank ba2 = new bank();
+        ba2.display(); 
+        ba2 = -ba1;
+        ba2.display();
+        Console.ReadLine();
+        }
+    }
+
+    class bank
+{
+     int x;
+     int y;
+    public bank(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+    public bank()
+    {   
+    }
+    public void display()
+    {
+        Console.Write(" " + x);
+        Console.Write(" " + y);
+        Console.WriteLine();
+    }
+    public static bank operator -(bank b)
+    {
+        b.x = -b.x;
+        b.y = -b.y;
+        return b;
+    }
+}
+}
---
+++ Day14/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Write a C# program to demonstrate method overloading */
+
+            int a=add(5,5);
+            int b=add(5,5,5);
+            double c=add(5.26,4.32,21.1,52.42);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+
+        public static int add(int a, int b)  
+        {    
+        return a + b;    
+        
+        }    
+        public static int add(int a, int b,int c) 
+        {    
+            return a + b + c;    
+        
+        }    
+        public static double add(double a, double b,double c,double d) 
+        {    
+            return a + b + c + d;    
+        
+        }    
+
+
+
+    }
+}
